b47392c669e416d4f81a775c1071ef1f242ff9bb
Logan Meyers <theterrarian03@gmail.com>
2025-09-06 13:27:58 -0700

    223 example updates

---
+++ 223/AnalyzingAlgorithms/hanoi.cpp
@@ -0,0 +1,6 @@
+// steps for solving:
+// 1. move n-1 disks from A to B (may use C as tmp)
+//    - C as helper
+// 2. move final disk to C
+// 3. move n-1 disks from peg b to c
+//    - A as helper
